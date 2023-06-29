4e17ae5ae8c7793746182d5e6d43a6089e138393
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-29 21:32:43 +0800

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,17 @@ jQuery(document).ready(function ($) {
   elementor_slider();
   latest_post();
   footer_accordion();
+  anchor_section();
 });
 
+function anchor_section() {
+  jQuery('.anchor-section').click(function (e) {
+    $target = jQuery(this).find('.elementor-button-link').attr('href');
+    console.log($target);
+    window.location.href = $target;
+
+  });
+}
 function footer_accordion() {
 
   if (window.innerWidth < 768) {
