52abebbf0dc5597debc34439ec7f8c20899add6c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-30 17:48:57 +0800

    Update page-header.php

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -112,6 +112,9 @@ if (function_exists('ciyashop_is_woocommerce_page') && ciyashop_is_woocommerce_p
           <?php if (is_product_category()) { ?>
 
             <?php
+            $term = get_queried_object();
+
+            var_dump($term);
             echo term_description(get_the_ID());
             ?>
             <div class="col-md-12 text-center">
