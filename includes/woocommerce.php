8046305365f1f6947dada5df7a6960fa33f679a0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-11 21:49:47 +0800

    cscs

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -209,22 +209,24 @@ function add_text_after_excerpt_single_product($post_excerpt)
     'fire_protection', 'drop_test', 'ventilation', 'temperature', 'doors', 'locking', 'construction', 'power', 'shelving', 'multipoint_lock', 'keypad', 'alarm', 'water_resist', 'combination', 'laptop', 'keyhole', 'fingerprint', 'insurance'
   );
 ?>
-  <ul>
-    <?php foreach ($specs as $spec) { ?>
-      <?php
-      $spec_val = carbon_get_the_post_meta($spec);
-      ?>
-      <?php if ($spec_val) { ?>
+  <div class="specs-box">
+    <ul>
+      <?php foreach ($specs as $spec) { ?>
+        <?php
+        $spec_val = carbon_get_the_post_meta($spec);
+        ?>
+        <?php if ($spec_val) { ?>
 
-        <li class="d-flex">
-          <span class="icon"><?= $SVG->$spec ?></span>
-          <span class="text"><?= $spec_val ?> </span>
-        </li>
+          <li class="d-flex align-items-center">
+            <span class="icon"><?= $SVG->$spec ?></span>
+            <span class="text"><?= $spec_val ?> </span>
+          </li>
 
+        <?php } ?>
       <?php } ?>
-    <?php } ?>
 
-  </ul>
+    </ul>
+  </div>
 <?php
   return ob_get_clean();
 }
