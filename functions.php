6b2a9d8ad778b22da10e443d1eb48e4bad3577b8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-11 19:22:02 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -201,3 +201,23 @@ function get_posts_details($post_type, $label = 'Select Post', $posts_per_page =
 
 	return $return;
 }
+
+
+function prefix_auto_featured_image() {
+    global $post;
+    if (!has_post_thumbnail($post->ID)) {
+        $attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
+         
+      if ($attached_image) {
+              foreach ($attached_image as $attachment_id => $attachment) {
+                   set_post_thumbnail($post->ID, $attachment_id);
+              }
+         }
+    }
+}
+add_action('the_post', 'prefix_auto_featured_image');
+add_action('save_post', 'prefix_auto_featured_image');
+add_action('draft_to_publish', 'prefix_auto_featured_image');
+add_action('new_to_publish', 'prefix_auto_featured_image');
+add_action('pending_to_publish', 'prefix_auto_featured_image');
+add_action('future_to_publish', 'prefix_auto_featured_image');
