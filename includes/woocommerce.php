94c8ec2a0773b06be1e576bb5e82a92845caa751
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-11 18:15:51 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -189,7 +189,6 @@ function woo_stockists_tab_content()
         if (array_key_exists($product_id, $product_arr)) {
           echo get_the_title();
         }
-        echo get_the_ID();
         echo '</li>';
       }
       echo '<pre>';
