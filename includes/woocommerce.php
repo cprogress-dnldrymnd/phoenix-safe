c8c357dd3768524b78010fe509ac6ad208a808c9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-11 21:47:13 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -217,8 +217,8 @@ function add_text_after_excerpt_single_product($post_excerpt)
       <?php if ($spec_val) { ?>
 
         <li class="d-flex">
-          <span class="icon"><?= $SVG->$key ?></span>
-          <span class="text"><?= $spec_val ?> <?= $spec ?> </span>
+          <span class="icon"><?= $SVG->$spec ?></span>
+          <span class="text"><?= $spec_val ?> </span>
         </li>
 
       <?php } ?>
