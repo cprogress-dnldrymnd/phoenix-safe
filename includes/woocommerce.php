bbca1aa544072ff8002720feaf3c444167ba35e4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-11 21:37:24 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -204,8 +204,11 @@ function add_text_after_excerpt_single_product($post_excerpt)
 {
   ob_start();
   echo $post_excerpt;
+  $specs = array(
+    'fire_protection', 'drop_test', 'ventilation', 'temperature', 'doors', 'locking', 'construction', 'power', 'shelving', 'multipoint_lock', 'keypad', 'alarm', 'water_resist', 'combination', 'laptop', 'keyhole', 'fingerprint', 'insurance'
+  );
 ?>
-dsdsds
+
 
 <?php
   return ob_get_clean();
